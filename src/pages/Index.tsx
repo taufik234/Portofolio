34755b885537b1d914e57cfcd983b25c47d212ba
taufik234 <taufiksuryo345@gmail.com>
2025-10-17 21:42:35 +0700

    add about

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,12 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 const Index = () => {
   return (
     <div className="min-h-screen w-full">
       <Navigation />
       <Hero />
+      <About />
     </div>
   );
 };
